b7206ee239c63feff1d9b8c9c3cba6c11086084a
Beth <morris.bethany66@gmail.com>
2020-09-05 23:30:55 -0600

    removed files

--- WordSearch/Models/WordSearch.swift
+++ WordSearch/Models/WordSearch.swift
@@ -8,230 +8,187 @@
 
 import Foundation
 
-typealias Grid = [[Character]]
-
-/// Helper class to get a random alpha char
-fileprivate class CharacterRandomizer {
-    private var allLetters = (65...90).map { Character(Unicode.Scalar($0)) }
-    func randomChar() -> Character {
-        return allLetters.randomElement()!
+enum PlacementType: CaseIterable {
+    
+    case leftRight
+    case rightLeft
+    case upDown
+    case downUp
+    case topLeftBottomRight
+    case topRightBottomLeft
+    case bottomLeftTopRight
+    case bottomRightTopLeft
+    
+    var movement: (x: Int, y: Int) {
+        switch self {
+        case .leftRight:
+            return (1,0)
+        case .rightLeft:
+            return (-1, 0)
+        case .upDown:
+            return (0, 1)
+        case .downUp:
+            return (0, -1)
+        case .topLeftBottomRight:
+            return (1, 1)
+        case .topRightBottomLeft:
+            return (-1, 1)
+        case .bottomLeftTopRight:
+            return (1, -1)
+        case .bottomRightTopLeft:
+            return (-1, -1)
+        }
     }
 }
 
-/// Alias for a 2d-array
-/// with a helper function to fill out placeholders with random chars.
-fileprivate extension Grid {
-    func randomizePlaceHolders() -> Grid {
-        var grid = self
-        guard !self.isEmpty && !self[0].isEmpty else {
-            return grid
+enum Difficulty {
+    case easy
+    case medium
+    case hard
+    
+    var placementTypes: [PlacementType] {
+        switch self {
+        case .easy:
+            return [.leftRight, .upDown].shuffled()
+        case .medium:
+            return [.leftRight, .rightLeft, .upDown, .downUp].shuffled()
+        case .hard:
+            return PlacementType.allCases.shuffled()
         }
-        let randomizer = CharacterRandomizer()
-        for i in 0..<self.count {
-            for j in 0..<self[0].count {
-                if grid[i][j] == WordGridGenerator.placeholder {
-                    grid[i][j] = randomizer.randomChar()
+    }
+}
+
+//struct Word: Decodable {
+//    var text: String
+//}
+
+class Label {
+    var letter: Character = " "
+}
+
+class WordSearch {
+    
+    let allLetters = (65...90).map { Character(Unicode.Scalar($0)) }
+    //var words = [Word]()
+    var words = ["Swift", "Kotlin", "ObjectiveC", "Variable", "Java", "Mobile"]
+    var gridSize = 10
+    var labels = [[Label]]()
+    var difficulty = Difficulty.hard
+    //var numOfPages = 10
+    
+    var letters = [Character]()
+    
+    // MARK: - Methods
+    
+    func makeGrid() {
+        labels = (0 ..< gridSize).map { _ in
+            (0 ..< gridSize).map { _ in Label() }
+        }
+            
+        placeWords()
+        fillGaps()
+        printGrid()
+    }
+    
+    private func fillGaps() {
+        for column in labels {
+            for label in column {
+                if label.letter == " " {
+                    label.letter = allLetters.randomElement()!
                 }
             }
         }
-        return grid
     }
-}
-
-class WordGridGenerator {
-
-    /// Private helper struct
-    /// moving steps of a direction
-    /// e.g. an up-left direction should
-    /// move the row by -1
-    /// and the col by -1
-    private struct MovingStep {
-        var stepRow: Int
-        var stepCol: Int
-    }
-
-    private enum Direction: CaseIterable {
-        case up
-        case down
-        case left
-        case right
-        case upLeft
-        case upRight
-        case downLeft
-        case downRight
-
-        func getMovingSteps() -> MovingStep {
-            switch self {
-            case .up:
-                return MovingStep(stepRow: -1, stepCol: 0)
-            case .down:
-                return MovingStep(stepRow: 1, stepCol: 0)
-            case .left:
-                return MovingStep(stepRow: 0, stepCol: -1)
-            case .right:
-                return MovingStep(stepRow: 0, stepCol: 1)
-            case .upLeft:
-                return MovingStep(stepRow: -1, stepCol: -1)
-            case .upRight:
-                return MovingStep(stepRow: -1, stepCol: 1)
-            case .downLeft:
-                return MovingStep(stepRow: 1, stepCol: -1)
-            case .downRight:
-                return MovingStep(stepRow: 1, stepCol: 1)
+    
+    private func printGrid() {
+        for column in labels {
+            for row in column {
+                print(row.letter, terminator: "")
+                letters.append(row.letter)
             }
+            
+            print("")
         }
     }
-
-    /// Private class that holds a state of a grid
-    private class State {
-        /// Current grid
-        var grid: Grid
-
-        /// Remaining words.
-        /// Should pick a word and attempt to assign to the grid
-        var words: [String]
-
-        /// Available directions to try
-        var directions: [Direction]
-
-        /// Available positions to try
-        var positions: [Int]
-
-        init(grid: Grid, words: [String], directions: [Direction], positions: [Int]) {
-            self.grid = grid
-            self.words = words
-            self.directions = directions
-            self.positions = positions
-        }
-    }
-
-    var words: [String] = []
-    // Key format: "startRow:startCol:endRow:endCol"
-    // Value: the corresponding word
-    // We will fill this map during grid generation
-    // This is efficient to get back the word when user is swiping
-    // Where we only know the start and end position of the line.
-    var wordsMap: [String: String] = [:]
-    var nRow: Int = 10
-    var nCol: Int = 10
-
-    private var directions = Direction.allCases
-
-    init(words: [String], row: Int, column: Int) {
-        self.words = words
-        self.nRow = row
-        self.nCol = column
-    }
-
-    static let placeholder: Character = "#"
-
-    // We declare this to be a static func to be used by other classes too
-    // We can also define this as an extension method of String
-    static func wordKey(for startPos: Position, and endPos: Position) -> String {
-        return "\(startPos.row):\(startPos.col):\(endPos.row):\(endPos.col)"
-    }
-
-    /// Just a helper function to compute the key from positions
-    /// and add the word to the words map.
-    private func add(word: String, startPos: Position, endPos: Position) {
-        wordsMap[WordGridGenerator.wordKey(for: startPos, and: endPos)] = word
-    }
-
-    /// Attempt to assign a word to a grid starting from a position,
-    /// moving along a direction.
-    /// If we could assign the word, return the last position of the last char
-    /// To compute the words map.
-    private func assignWord(
-        grid: Grid,
-        word: String,
-        direction: Direction,
-        position: Int) -> (grid: Grid, lastPosition: Position)?  {
-        var dupGrid = grid
-        var row = position / nRow
-        var col = position % nCol
-        var idx = 0
-        let chars = Array(word)
-        let mStep = direction.getMovingSteps()
-        var lastPos = Position(row: row, col: col)
-        while true {
-            if idx == chars.count {
-                // Here the word has been assigned correctly.
-                break
-            }
-            // The position is in the grid
-            if row < 0 || row >= nRow || col < 0 || col >= nCol {
-                return nil
-            }
-            // This line passes the check above, so the position is valid.
-            lastPos = Position(row: row, col: col)
-
-            // If the current char is a "#" or it equals to the char in the grid,
-            // Then we are fine to move forward.
-            if dupGrid[row][col] == WordGridGenerator.placeholder || dupGrid[row][col] == chars[idx] {
-                // update the grid.
-                dupGrid[row][col] = chars[idx]
-                //move along the direction
-                row += mStep.stepRow
-                col += mStep.stepCol
-                idx += 1
+    
+    private func labels(fromX x: Int, y: Int, word: String, movement: (x: Int, y: Int)) -> [Label]? {
+        var returnValue = [Label]()
+        var xPosition = x
+        var yPosition = y
+        
+        for letter in word {
+            let label = labels[xPosition][yPosition]
+            
+            if label.letter == " " || label.letter == letter {
+                returnValue.append(label)
+                xPosition += movement.x
+                yPosition += movement.y
             } else {
-                // if it fails at an any step, return nil
                 return nil
             }
         }
-        return (grid: dupGrid, lastPosition: lastPos)
+        
+        return returnValue
     }
-
-    /// Main method of the class.
-    /// This generates a 2-d grid containing words in random positions and directions.
-    public func generate() -> Grid? {
-        let empty = Grid(repeating: [Character](repeating: WordGridGenerator.placeholder, count: nCol), count: nRow)
-        let positions = Array(0..<(nRow * nCol)).shuffled()
-        let initialState = State(
-            grid: empty,
-            words: words,
-            directions: directions,
-            positions: positions.shuffled()
-        )
-        var states: [State] = [initialState]
-
-        while true {
-            guard let state = states.last else {
-                // If we popped out every state, then there's no solution for such a grid.
-                return nil
-            }
-            // Pop and try the last direction
-            var direction = state.directions.last
-            state.directions = state.directions.dropLast()
-            if direction == nil {
-                // No direction worked at the position
-                // So we try next position
-                state.positions = state.positions.dropLast()
-                state.directions = Direction.allCases.shuffled()
-                direction = state.directions.last!
-            }
-            let pos = state.positions.last
-            if pos == nil {
-                states = states.dropLast()
-            } else {
-                if state.words.isEmpty {
-                    // If we assigned all words, grid is generated.
-                    break
-                }
-                //We are sure that we have remaining words.
-                let word = state.words.last!
-                if let (grid, lastPos) = assignWord(grid: state.grid, word: word, direction: direction!, position: pos!) {
-                    states.append(State(grid: grid, words: state.words.dropLast(), directions: Direction.allCases.shuffled(), positions: positions))
-                    // Compute the words map
-                    let posRow = pos! / nRow
-                    let posCol = pos! % nCol
-                    add(word: word, startPos: Position(row: posRow, col: posCol), endPos: lastPos)
+    
+    private func tryPlacing(_ word: String, movement: (x: Int, y: Int)) -> Bool {
+        let xLength = (movement.x * (word.count - 1))
+        let yLength = (movement.y * (word.count - 1))
+        
+        let rows = (0 ..< gridSize).shuffled()
+        let columns = (0 ..< gridSize).shuffled()
+        
+        for row in rows {
+            for column in columns {
+                let finalX = column + xLength
+                let finalY = row + yLength
+                
+                if finalX >= 0 && finalX < gridSize && finalY >= 0 && finalY < gridSize {
+                    if let returnValue = labels(fromX: column, y: row, word: word, movement: movement) {
+                        for (index, letter) in word.enumerated() {
+                            returnValue[index].letter = letter
+                        }
+                        
+                        return true
+                    }
                 }
             }
         }
-        return states.last!.grid.randomizePlaceHolders()
+        
+        return false
     }
-}
-
-
-
+    
+    //double check that this works
+    private func place(_ word: String) -> Bool {
+        let formattedWord = word.replacingOccurrences(of: " ", with: "").uppercased()
+        
+        for type in difficulty.placementTypes {
+            if tryPlacing(formattedWord, movement: type.movement) {
+                return true
+            }
+        }
+        
+//        return difficulty.placementTypes.contains {
+//            tryPlacing(formattedWord, movement: $0.movement)
+//        }
+        
+        return false
+    }
+    
+    private func placeWords() -> [String] {
+        words.shuffle()
+        
+        var usedWords = [String]()
+        
+        for word in words {
+            if place(word) {
+                usedWords.append(word)
+            }
+        }
+        
+        return usedWords
+        
+        //return words.shuffled().filter(place)
+    }
+    
+} //End
